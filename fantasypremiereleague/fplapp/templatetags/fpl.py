82bfbb5063b29ed78772650af9aef3d1a491411c
keshab <keshab_budhathoky@yahoo.co.uk>
2019-10-20 10:04:25 -0400

    Initial tag commit

--- fantasypremiereleague/fplapp/templatetags/fpl.py
+++ fantasypremiereleague/fplapp/templatetags/fpl.py
@@ -1,6 +1,4 @@
 from django import template
-from fplapp.fixtures import Fixtures
-import asyncio
 
 register = template.Library()
 
@@ -9,6 +7,31 @@ register = template.Library()
 def gameweek_filter(game_week, match_fixtures):
     return list(filter(lambda fixture: fixture['event'] == game_week, match_fixtures))
 
+
 @register.simple_tag()
 def team_short_name(team_id, teams):
     return teams[team_id]
+
+
+@register.simple_tag()
+def player_name(player_id, players):
+    player_info = list(filter(lambda player: player['id'] == player_id, players))
+    player_full_name = "{} {}".format(player_info[0]['first_name'], player_info[0]['second_name'])
+    return player_full_name
+
+
+@register.filter()
+def price(cost):
+    return cost / 10
+
+
+@register.filter()
+def position(element_type):
+    element_types = {1: 'Goal Keeper', 2: 'Defender', 3: 'Midfielder', 4: 'Striker'}
+    return element_types[element_type]
+
+
+@register.filter()
+def status(status_code):
+    status_codes = {'d': 'Doubtful', 'i': 'Injured', 's': 'Suspended'}
+    return status_codes[status_code]
